912c2b05f73a35cb0acb94b21e450ba77e5381f9
velllum <velllum@yandex.ru>
2021-11-22 21:16:21 +0300

    review

--- main.py
+++ main.py
@@ -1,84 +1,97 @@
-import datetime as dt
+from datetime import datetime as dt
 
 
 class Record:
+    """- Комментарий"""
     def __init__(self, amount, comment, date=''):
         self.amount = amount
-        self.date = (
-            dt.datetime.now().date() if
-            not
-            date else dt.datetime.strptime(date, '%d.%m.%Y').date())
+        self.date = dt.now().date() if not date else dt.strptime(date, '%d.%m.%Y').date()
         self.comment = comment
 
 
 class Calculator:
+    """- Комментарий"""
     def __init__(self, limit):
         self.limit = limit
         self.records = []
 
     def add_record(self, record):
+        """- Комментарий"""
         self.records.append(record)
 
     def get_today_stats(self):
+        """- Комментарий"""
         today_stats = 0
-        for Record in self.records:
-            if Record.date == dt.datetime.now().date():
-                today_stats = today_stats + Record.amount
+
+        for record in self.records:
+            if record.date == dt.now().date():
+                today_stats += record.amount
+
         return today_stats
 
     def get_week_stats(self):
+        """- Комментарий"""
         week_stats = 0
-        today = dt.datetime.now().date()
+        today = dt.now().date()
+
         for record in self.records:
-            if (
-                (today - record.date).days < 7 and
-                (today - record.date).days >= 0
-            ):
+            if 7 > (today - record.date).days >= 0:
                 week_stats += record.amount
+
         return week_stats
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained(self):  # Получает остаток калорий на сегодня
+    """- Комментарий"""
+    def get_calories_remained(self):
+        """- Получает остаток калорий на сегодня"""
         x = self.limit - self.get_today_stats()
+
         if x > 0:
-            return f'Сегодня можно съесть что-нибудь' \
-                   f' ещё, но с общей калорийностью не более {x} кКал'
+            return f'''
+            Сегодня можно съесть что-нибудь ещё,
+            но с общей калорийностью не более {x} кКал'''
+
         else:
-            return('Хватит есть!')
+            return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
+    """- Комментарий"""
     USD_RATE = float(60)  # Курс доллар США.
     EURO_RATE = float(70)  # Курс Евро.
 
-    def get_today_cash_remained(self, currency, USD_RATE=USD_RATE, EURO_RATE=EURO_RATE):
+    def get_today_cash_remained(self, currency, usd_rate=USD_RATE, euro_rate=EURO_RATE):
+        """- Комментарий"""
         currency_type = currency
         cash_remained = self.limit - self.get_today_stats()
 
         if currency == 'usd':
-            cash_remained /= USD_RATE
+            cash_remained /= usd_rate
             currency_type = 'USD'
 
         elif currency_type == 'eur':
-            cash_remained /= EURO_RATE
+            cash_remained /= euro_rate
             currency_type = 'Euro'
 
         elif currency_type == 'rub':
-            cash_remained == 1.00
-            currency_type = 'руб'
+            cash_remained = 1.00
+            currency_type = 'Руб'
 
         if cash_remained > 0:
-            return (
-                f'На сегодня осталось {round(cash_remained, 2)} '
-                f'{currency_type}'
-            )
+            result = round(cash_remained, 2)
+            return f'''
+            На сегодня осталось {result} 
+            {currency_type}'''
+
         elif cash_remained == 0:
-            return 'Денег нет, держись'
+            return 'Денег нет, но вы держитесь ;)'
 
         elif cash_remained < 0:
-            return 'Денег нет, держись:' \
-                   ' твой долг - {0:.2f} {1}'.format(-cash_remained, currency_type)
+            return f'''
+            Денег нет, но вы держитесь ;) : 
+            твой долг - {-cash_remained:.2f} {currency_type}'''
 
     def get_week_stats(self):
+        """- Комментарий"""
         super().get_week_stats()
